229cf4f15bd8aa238c703795b0ec39385b7ae712
Adam Hubbs <amhubbs17@gmail.com>
2025-06-09 14:25:55 -0600

    clean up grouped header and class order

--- R/as_freq.R
+++ R/as_freq.R
@@ -29,7 +29,20 @@ as_freq_y2 <- function(df, p = NULL) {
     attr(df, "prompts") <- p
   }
 
-  class(df) <- c("freq_y2", class(df))
+  # IF the data frame is grouped, insert the freq_y2 class AFTER the grouped_df class.
+  # This is necessary for proper dispatch.
+  # We don't have to worry about grouped_freq_y2 already existing here becasue if 'freq_y2' was found earlier, it just returns it.
+  if ('grouped_df' %in% class(df)) {
+    class(df) <- c(
+      'grouped_freq_y2',
+      'grouped_df',
+      'freq_y2',
+      class(df)[class(df) != 'grouped_df']
+    )
+  } else {
+    class(df) <- c("freq_y2", class(df))
+  }
+
   return(df)
 }
 
@@ -65,9 +78,64 @@ group_by.freq_y2 <- function(.data, ..., add = FALSE, .drop = dplyr::group_by_dr
   as_freq_y2(NextMethod(), p = prompts)
 }
 
+#' @exportS3Method dplyr::ungroup
+ungroup.grouped_freq_y2 <- function(.data, ..., add = FALSE, .drop = dplyr::group_by_drop_default(.data)) {
+  prompts <- attr(.data, "prompts")
+  class(.data) <- setdiff(class(.data), c("grouped_freq_y2", "freq_y2"))
+  as_freq_y2(NextMethod(), p = prompts)
+}
 
 
+#' @exportS3Method dplyr::dplyr_col_modify
+dplyr_col_modify.grouped_freq_y2 <- function(data, cols) {
+  prompts <- attr(.data, "prompts")
 
+  out <- dplyr_col_modify(as_tibble(data), cols)
+  if (any(names(cols) %in% group_vars(data))) {
+    temp <- grouped_df(out, group_vars(data), drop = group_by_drop_default(data))
+  } else {
+    temp <- new_grouped_df(out, group_data(data))
+  }
+ as_freq_y2(temp, prompts)
+}
+
+dplyr_col_select <- function (.data, loc, error_call = caller_env())
+{
+  loc <- vec_as_location(loc, n = ncol(.data), names = names(.data))
+  out <- .data[loc]
+
+  print("Attributes after selection -------")
+  print(attributes(out))
+
+  if (!inherits(out, "data.frame")) {
+    classes_data <- glue_collapse(class(.data), sep = "/")
+    classes_out <- glue_collapse(class(out), sep = "/")
+    bullets <- c("Can't reconstruct data frame.", x = glue("The `[` method for class <{classes_data}> must return a data frame."),
+                 i = glue("It returned a <{classes_out}>."))
+    abort(bullets, call = error_call)
+  }
+  if (length(out) != length(loc)) {
+    classes_data <- glue_collapse(class(.data), sep = "/")
+    classes_out <- glue_collapse(class(out), sep = "/")
+    s <- function(x) if (length(x) == 1)
+      ""
+    else "s"
+    bullets <- c("Can't reconstruct data frame.", x = glue("The `[` method for class <{classes_data}> must return a data frame with {length(loc)} column{s(loc)}."),
+                 i = glue("It returned a <{classes_out}> of {length(out)} column{s(out)}."))
+    abort(bullets, call = error_call)
+  }
+  if (identical(class(.data), "data.frame") || identical(class(.data),
+                                                         c("data.table", "data.frame"))) {
+    out <- dplyr_reconstruct(out, .data)
+  }
+  out
+}
+
+# dplyr_col_modify for Data.frame
+# does a new_data_frame (vctrs) then a dplyr_reconstruct
+
+
+# For grouped it does a col_modify (as_tibble(data)) then either does a grouped_df() or a new_grouped_df() [Both of which eventually end up as new_grouped_df]
 
 
 
--- R/printing.R
+++ R/printing.R
@@ -25,11 +25,7 @@ print_freq_inf <- function() {
 
 #' @exportS3Method pillar::tbl_sum
 tbl_sum.freq_y2 <- function(x, ...) {
-  header <- c(`A frequency tibble` = pillar::dim_desc(x))
-  if('grouped_df' %in% class(x)) {
-    header <- c(header, c(Groups = dplyr:::group_sum(x)))
-  }
-  return(header)
+  c(`A frequency tibble` = pillar::dim_desc(x))
 }
 
 
